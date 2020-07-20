177c03bc05d54f5dbe3d8d35fc653665a420e2cf
ivxxi <crystalalice21@gmail.com>
2020-07-20 05:41:18 +0300

    adds ub views

--- uber/views.py
+++ uber/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.conf.urls import url,include
 # Create your views here.
+
+
+def login(request):
+    return render(request, "registration/registration_form.html")
