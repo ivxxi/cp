829df4e85786c9dfb3f2e21c7e3367d5dd81e3ed
ivxxi <crystalalice21@gmail.com>
2020-07-20 05:00:42 +0300

    adds model class Driver

--- uber/models.py
+++ uber/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Driver(models.Model):
+    name = models.OneToOneField(User,on_delete=models.CASCADE)
+    bio = models.CharField(max_length=60)
+    avatar = models.ImageField(upload_to='ProfilePicture/')
+    vehicle = models.ForeignKey('uber.Car', on_delete=models.CASCADE)
+    pickup_location = models.ForeignKey('uber.Location', on_delete=models.CASCADE)
+    contact_info = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.user.username 
