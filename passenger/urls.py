15df5f798fcf75376e8b38d525265028eb2bd068
ivxxi <crystalalice21@gmail.com>
2020-07-20 04:51:03 +0300

    adds urls to url.py files

--- passenger/urls.py
+++ passenger/urls.py
@@ -8,4 +8,8 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     path('^$',views.landing,name = 'landing'),
+    path('passenger/home',views.passenger, name = 'passenger home page'),
+    path('passenger/profile',views.pprofile, name = 'passenger profile page'),
+    path('passenger/destination',views.pdestination, name = 'passenger pick destination page'),
+    path('about',views.about, name = 'about page for app'),
 ]
