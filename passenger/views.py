d54807f032fba82df2afeababd84c6c1969c0fe9
ivxxi <crystalalice21@gmail.com>
2020-07-20 04:54:13 +0300

    adds passenger views

--- passenger/views.py
+++ passenger/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+
+def landing(request):
+    return render(request, 'landingpage/land-page.html')
+
+def passenger(request):
+    return render(request, 'passenger/home.html')
+
+def pprofile(request):
+    return render(request, 'passenger/profile.html')
+
+def pdestination(request):
+    return render(request, 'passenger/destination.html')
+
+def about(request):
+    return render(request, 'uber/about.html')
